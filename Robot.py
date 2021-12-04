ea5fd4620c618ded412755aa5844189f7bf9a5ba
Wojtek <wojtek.golebiowski98@gmail.com>
2021-12-04 22:49:52 +0100

    Dodano kolor jako atrybut robota, funkcje set_path set_position get_color set_color

--- Robot.py
+++ Robot.py
@@ -4,13 +4,14 @@ from Astar import Astar
 class Robot:
     """Klasa modelujaca robota"""
 
-    def __init__(self, position=None):
+    def __init__(self, position=None, color='b'):
         if position:
             self.position = position
         else:
             self.position = 0, 0
         self.status = "Free"
         self.path = []
+        self.color = color
 
     def find_path(self, goal):
         self.path = Astar().solve(self.position, goal)
@@ -22,8 +23,20 @@ class Robot:
     def get_path(self):
         return self.path
 
+    def set_path(self, path):
+        self.path = path
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
     def get_position(self):
         return self.position
+
+    def set_position(self, pos):
+        self.position = pos
 
 
 if __name__ == "__main__":
